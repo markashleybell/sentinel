73268d889ad2a7e91f748a4901735897743fd82e
Mark Bell <me@markashleybell.com>
2015-03-04 07:20:25 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sentinel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watchDirectories = new List<string> {
+                
+            };
+
+            watchDirectories.ForEach(path => CreateWatcher(path));
+
+            Console.ReadLine();
+        }
+
+        static void CreateWatcher(string path)
+        {
+            var watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+            // watcher.NotifyFilter = NotifyFilters.
+
+            watcher.Created += watcher_Created;
+            watcher.Changed += watcher_Changed;
+            watcher.Deleted += watcher_Deleted;
+            watcher.Renamed += watcher_Renamed;
+        }
+
+        static void WriteLogLine(FileSystemEventArgs e)
+        {
+            Console.WriteLine("{0,10}     {1,-10}", e.ChangeType.ToString().ToUpper(), e.FullPath);
+        }
+
+        static void watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            WriteLogLine(e);
+        }
+
+        static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            WriteLogLine(e);
+        }
+
+        static void watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            WriteLogLine(e);
+        }
+
+        static void watcher_Renamed(object sender, FileSystemEventArgs e)
+        {
+            WriteLogLine(e);
+        }
+    }
+}
